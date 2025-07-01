146331d9b47cba683f68ad731cdc6336f6d86f0f
Tauã Lima <131001303+taua-lima-dev@users.noreply.github.com>
2025-07-01 12:43:29 -0300

    Update bttGenerate.js
    código refatorado

--- js/bttGenerate.js
+++ js/bttGenerate.js
@@ -2,14 +2,8 @@ import generateText from "./generateText.js";
 
 function bttGenerate() {
     const bttGenerate = document.getElementById('bttGenerate');
-    // const texto = document.querySelector('.texto');
-    // const container = document.querySelector('.container');
-    // const botoes = document.querySelector('.botoes')
 
-bttGenerate.addEventListener("click", function (e) {
-    // texto.classList.remove('oculto')
-    // botoes.classList.remove('oculto')
-    // container.classList.add('oculto')
+    bttGenerate.addEventListener("click", function (e) {
     generateText();
     e.preventDefault();
 });
